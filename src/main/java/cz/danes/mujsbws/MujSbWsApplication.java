c74b3ac6c659f2b7bcd01c3d2f698a0e8759c9a7
“DZCM4G4” <danielvacha.dv@gmail.com>
2023-10-19 21:37:58 +0200

    default commit echo service work ok

---
+++ src/main/java/cz/danes/mujsbws/MujSbWsApplication.java
@@ -0,0 +1,13 @@
+package cz.danes.mujsbws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MujSbWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MujSbWsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/danes/mujsbws/MujSbWsApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.danes.mujsbws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MujSbWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
